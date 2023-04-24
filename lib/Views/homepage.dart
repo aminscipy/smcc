3518953e5b8cfa7f77a9979623ba25ea2f2e6b0f
amin <thisisaamin@gmail.com>
2023-04-24 23:53:30 +0530

    tts working

--- lib/Views/homepage.dart
+++ lib/Views/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:text_to_speech/text_to_speech.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,9 +8,29 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+TextToSpeech tts = TextToSpeech();
+
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    tts.setRate(0.9);
+    tts.getDisplayLanguages();
+    tts.setLanguage('hindi');
+    tts.setPitch(0.8);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: IconButton(
+            onPressed: () {
+              tts.speak(
+                  'इस लेख में आप दुनिया के एक बहुत महान शासक सिकंदर महान का इतिहास के बारे में बताने वाले है। जिसमे हम आपको इनके जीवन की पूरी कहानी (Great Story) देंगे और साथ ही इनकी द्वारा लड़े गए कुछ महत्वपूर्ण युद्धों के बारे में बात करेंगे।');
+            },
+            icon: const Icon(Icons.play_arrow)),
+      ),
+    );
   }
 }
